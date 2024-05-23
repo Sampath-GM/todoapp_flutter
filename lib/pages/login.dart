162274faa7564228815b297a4b1b0f7803f89d49
Sampath-GM <sampathgm0341@gmail.com>
2024-05-23 22:23:25 +0530

    modified loginpage ath

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todoapp/pages/HomePage.dart';
 
 class Loginpage extends StatelessWidget {
   const Loginpage({super.key});
@@ -7,71 +8,71 @@ class Loginpage extends StatelessWidget {
   Widget build(BuildContext context) {
     return  Scaffold( 
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.all(.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Image.asset(
-                'assets/images/Login.png',
-                height: 400,
-              ),
-              Padding(
-                padding: EdgeInsets.all(8.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Email',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(8.0)))
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Image.asset(
+                  'assets/images/Login.png',
+                  height: 400,
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(12.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Email',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(8.0)))
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(8.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Password',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(8.0)))
+              const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(8.0)))
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 20,),
-              Container(
-                child:   ElevatedButton(
-                onPressed: (){},
-                style: ButtonStyle(
-                  backgroundColor: WidgetStatePropertyAll(Color.fromARGB(255, 191, 77, 211)),
-                ), 
-                child: Text(
-                  'Login',
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                  )),
-                height: 40,
-                width: 250,
-              ),
-                SizedBox(height: 10,),
-                  //forgot password
-              InkWell(
-                child: Text(
-                  'Forgot Password?',
-                  style: TextStyle(
-               
-                  ),
+               const SizedBox(height: 20,),
+                Container(
+                  child: ElevatedButton(
+                  onPressed: (){
+                    Navigator.of(context).push(MaterialPageRoute(builder: (context)=> Homepage()));
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll(Color.fromARGB(255, 191, 77, 211)),
+                  ), 
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                    )),
+                  height: 40,
+                  width: 250,
                 ),
-              ),
-              
-            ],
-          )
+                 const SizedBox(height: 10,),
+                    //forgot password
+               const InkWell(
+                  child: Text(
+                    'Forgot Password?',
+                    style: TextStyle(
+                    ),
+                  ),
+                ),  
+              ],
+            )
+            ),
+        ),
           ),
-          ),
-          
-    
     );
   }
 }
